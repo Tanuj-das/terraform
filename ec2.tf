900cefe6c3af4ec4df85f097699af4420b628969
Tanuj-das <50992028+Tanuj-das@users.noreply.github.com>
2020-10-28 16:11:30 +0530

    Create ec2.tf
    Contains the ec2 terraform deployment script.

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  access_key = "ACCESS_KEY_HERE"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "tanuj-ec2" {
+  ami           = "ami-066663db63b3aa675"
+  instance_type = "t2.micro"
+  key_name = "terraform-key"
+  security_groups = ["${aws_security_group.allow_rdp.name}"]
+
+}
