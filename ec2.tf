ea298600eaaed518531961076bd518ed82d7566d
Tanuj-das <50992028+Tanuj-das@users.noreply.github.com>
2020-10-30 11:19:18 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,5 @@ provider "aws" {
 resource "aws_instance" "tanuj-ec2" {
   ami           = "ami-066663db63b3aa675"
   instance_type = "t2.micro"
-  key_name = "terraform-key"
-  security_groups = ["${aws_security_group.allow_rdp.name}"]
-
+ 
 }
